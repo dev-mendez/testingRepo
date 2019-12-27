570adb8353a0ad4ac36fafe8203bcd1caabb3aa3
Armando <an2m001@gmail.com>
2019-12-27 23:56:29 +0100

    las exercices

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-let a = 'Armando';
-console.log(a + ' + ' + a);
+// Is n divisible by x and y?
+// function isDivisible(n, x, y) {
+//   if ((n % x === 0) & (n % y === 0)) r = true;
+//   else r = false;
+//   return r;
+// }
+// console.log(isDivisible(10, 2, 4));
 
-a.anchor();
+// What is between?
+// function between(a, b) {
+//   let arr = [];
+//   for (a; a <= b; a++) {
+//     arr.push(a)
+//   }
+//   return arr;
+// }
+// console.log(between(-5, 25));
 
-let b = num => {
-  return a + b + num;
-};
+// Is the string uppercase?
+// String.prototype.isUpperCase = function() {
+//   return this.toUpperCase == this;
+// };
 
-b();
-let a = 'Juliana' 
+
